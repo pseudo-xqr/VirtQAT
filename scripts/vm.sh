539d315e89bcb83f7edfb1064d6429c9c0fa67ea
pseudo-xqr <littlerosesclustering@outlook.com>
2025-05-01 12:13:41 -0500

    Add: setup VM

--- scripts/vm.sh
+++ scripts/vm.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
+# Refer to (https://askubuntu.com/questions/1480090/how-to-install-ubuntu-22-04-as-guest-in-kvm)
 
-# Start VM
-virt-install --connect qemu:///system --ram 2048 -n guest --osinfo=ubuntu22.04 \
-    --disk path=/var/lib/libvirt/images/disk.qcow,size=5 \
-    --vcpus=1 --graphics none --import
+# Prerequisite: 
+# 1. Download the ISO image
+wget https://releases.ubuntu.com/focal/ubuntu-20.04.6-live-server-amd64.iso -P /var/lib/libvirt/images/
+
+# 2. Read the ISO image and mount to /mnt/vm
+mkdir /mnt/vm
+sudo mount -o loop /var/lib/libvirt/images/ubuntu-22.04.6-live-server-amd64.iso /mnt/vm
+
+# 3. Install new VM
+sudo virt-install --name guest --ram=4096 --vcpus=1 \
+ --hvm  --disk path=/var/lib/libvirt/images/server_image.img,size=15 \
+ --cdrom /var/lib/libvirt/images/ubuntu-20.04.6-live-server-amd64.iso \
+ --graphics none --console pty,target_type=serial --os-type linux \
+ --network network:default --force --debug \
+ --boot kernel=/mnt/vm/casper/vmlinuz,initrd=/mnt/vm/casper/initrd,kernel_args="console=ttyS0"
+
+# 4. After the installation, unmount the ISO image
+virsh destroy guest
+virsh edit guest
+# Del the following line:
+# <kernel>/home/user/mnt/casper/vmlinuz</kernel>
+# <initrd>/home/user/mnt/casper/initrd</initrd>
+# <cmdline>console=ttyS0</cmdline>
+
+# 5. Start the VM
+virsh start guest
+virsh --connect qemu:///system console guest
